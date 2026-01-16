8bea0ead79a9d7d4c03dd6773ca4c04f33bc6b4c
Cedwards <cedwards2527@gmail.com>
2026-01-16 14:19:39 -0500

    Add mongoose

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,3 +8,10 @@ const { PORT = 3001 } = process.env;
 app.listen(PORT, () => {
   console.log(`"Listening on port ${PORT}"`);
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch(console.error);
