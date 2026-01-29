1ed9b1bdac5b6ecaa5c7c6831a7efc1512771437
Cedwards <cedwards2527@gmail.com>
2026-01-29 11:00:58 -0500

    Update createUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,31 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
+  const { name, avatar, email, password } = req.body;
 
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
-      }
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error has occurred on the server." });
-    });
+  bcrypt.hash(password, 10).then((hash) => {
+    User.create({ name, avatar, email, password: hash })
+      .then((user) => {
+        const userObj = user.toObject();
+        delete userObj.password;
+        res.status(201).send(userObj);
+      })
+      .catch((err) => {
+        console.error(err);
+
+        if (err.code === 11000) {
+          return res.status(409).send({ message: "Email already exists" });
+        }
+        if (err.name === "ValidationError") {
+          return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+        }
+        return res
+          .status(SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
+      });
+  });
 };
 
 const getUsers = (req, res) => {
