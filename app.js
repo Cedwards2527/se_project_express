be622f3ad0555763a5f17fbe79143bb2496b94a3
Cedwards <cedwards2527@gmail.com>
2026-02-13 20:55:49 -0500

    Prepare project for migration to new laptop

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use("/", mainRouter);
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db", {})
   .then(() => {
-    //  console.log("Connected to MongoDB: wtwr_db");
+    console.log("Connected to MongoDB: wtwr_db");
     app.listen(PORT, () => {
-      // console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
