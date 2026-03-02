8bfe354848665ff6da359f72e9fa1541ffe28f6f
Cedwards2527 <cedwards2527@gmail.com>
2026-03-02 17:25:23 -0500

    Add logger.js to middlewares

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 
@@ -12,8 +13,10 @@ const { PORT = 3001 } = process.env;
 app.use(cors());
 app.use(express.json());
 
-
+app.use(requestLogger);
 app.use("/", mainRouter);
+
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
